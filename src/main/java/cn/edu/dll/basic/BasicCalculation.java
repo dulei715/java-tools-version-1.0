83dc051cd3d94fbcefa2d5b4a6014ee1292b724f
dulei715 <dulei715@126.com>
2026-04-15 12:05:21 +0800

    Saved by Mac-LeileiDu

--- src/main/java/cn/edu/dll/basic/BasicCalculation.java
+++ src/main/java/cn/edu/dll/basic/BasicCalculation.java
@@ -41,6 +41,18 @@ public class BasicCalculation {
         return result;
     }
 
+    public static Double get1Norm(Integer[] pointA, Double[] pointB) {
+        if (pointA.length != pointB.length) {
+            throw new RuntimeException("The dimensionality of two points are not equal!");
+        }
+        int len = pointA.length;
+        Double result = 0.0;
+        for (int i = 0; i < len; i++) {
+            result += Math.abs(pointA[i]-pointB[i]);
+        }
+        return result;
+    }
+
     public static int get1Norm(int[] pointA, int[] pointB) {
         if (pointA.length != pointB.length) {
             throw new RuntimeException("The dimensionality of two points are not equal!");
